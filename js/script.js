9c150af5558120c53fa6be42c3dfa1f53eb7904b
Владимир Вергазов <vergazov@unik.local>
2021-04-12 16:41:59 +0300

    4

--- js/script.js
+++ js/script.js
@@ -97,7 +97,6 @@ setClock('.timer', deadLine);
 
 const modalTrigger = document.querySelectorAll('[data-modal]');
 const modal = document.querySelector('.modal');
-const modalClose = document.querySelector('[data-close]');
 
 
 function opneModal(){ // открываем окно модалки
@@ -119,10 +118,8 @@ function closeModal(){
 };
 
 
-modalClose.addEventListener('click', closeModal)
-
 modal.addEventListener('click', (e) => {
-    if(e.target === modal){
+    if(e.target === modal || e.target.getAttribute('data-close') == ''){
     closeModal();
     }
 });
@@ -134,7 +131,7 @@ document.addEventListener('keydown', (e)=>{
 
 });
 
-// const modalTimerId = setTimeout(opneModal, 3000);
+const modalTimerId = setTimeout(opneModal, 50000);
 
 function showModalByScroll(){
     if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
@@ -207,7 +204,7 @@ class MenuCard {
         "img/tabs/elite.jpg",
         "elite",
         'Меню "Премиум"',
-        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
         12,
         '.menu .container',
         'menu__item',
@@ -217,14 +214,97 @@ class MenuCard {
     new MenuCard(
         "img/tabs/post.jpg",
         "post",
-        'Меню "Постное""',
-        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        'Меню "Постное"',
+        'Меню "Постное" - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
         6,
         '.menu .container',
         'menu__item',
         'big'
 
     ).render();
+
+    //forms
+
+    const forms = document.querySelectorAll('form');
+    const message = {
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо скоро мы с вами свяжемся',
+        failure: 'Что то пошло не так...'
+    };
+
+    forms.forEach(item => { // перебираем формы
+        postData(item);
+    });
+
+    function postData(form){
+        form.addEventListener('submit', (e) => {  // добавляем обработчик
+            e.preventDefault();
+
+            const statusMessage = document.createElement('img'); //создаем тег для  картинки загрузки статусов
+            statusMessage.src = message.loading;  // добавляем путь к картинке стстауса 
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `; // помещаем сообщение loading
+            // form.append(statusMessage); 
+            form.insertAdjacentElement('afterend', statusMessage);// добавляем сообщение в конец блока
+
+
+            const request = new XMLHttpRequest(); // запрос на сервер
+            request.open('POST', 'server.php'); // метод и адрес
+
+            request.setRequestHeader('Content-type', 'aplication/json'); //устанавливаем параметры
+            const formData = new FormData(form); //новый объект
+
+            const object = {}; // создаем пустой объект
+
+            formData.forEach(function(value,key){ // перебираем формдату и записываем в объект
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object); // переводим в json
+
+
+            request.send(json); // отправляем
+            request.addEventListener('load', () => { //вешаем обработчик на событие 200 и остальные 
+                if(request.status === 200) {
+                    console.log(request.response);
+                    showThanksModal(message.success);
+                    form.reset();
+                        statusMessage.remove();                 
+                } else {
+                    showThanksModal(message.failure);
+                }
+            });
+        });
+    }
+
+    // modal window success or fail
+
+    function showThanksModal(message) { //функция показа окна об отправке
+        const prevModalDialog = document.querySelector('.modal__dialog'); //выбираем блок с окном
+        prevModalDialog.classList.add('hide'); //скрываем содержимое
+        opneModal(); //вызываем функию открытия окна
+
+        const thanksModal = document.createElement('div'); //создаем блок
+        thanksModal.classList.add('modal__dialog') // добавляем ему класс
+        //помещаем внутрь содержимое
+        thanksModal.innerHTML = ` 
+        <div class="modal__content">
+            <div data-close class="modal__close">&times;</div>
+            <div class="modal__title">${message}</div>
+        </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal); //находим модал, добавляем блок с текстом
+        setTimeout(()=>{ // устанавливаем таймаут
+            thanksModal.remove(); //удаляем блок
+            prevModalDialog.classList.add('show'); // добавляем класс с видимостью
+            prevModalDialog.classList.remove('hide'); // убираем класс хайд
+            closeModal(); //закрываем окно по таймауту 4 секунды
+        }, 4000);
+    };
+
 });
 
 
